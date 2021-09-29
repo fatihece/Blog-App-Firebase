aea1e5c3de7e539ac51f9b6299f7295f4094c303
fatih <fatihenaz@gmail.com>
2021-09-29 21:00:20 +0300

    created firebase.js

--- src/utils/firebaseUtil.js
+++ src/utils/firebaseUtil.js
@@ -11,8 +11,8 @@ const firebaseUtil = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
   appId: process.env.REACT_APP_FIREBASE_appId,
 });
-export default firebaseUtil;
 
+export default firebaseUtil;
 export const auth = firebaseUtil.auth();
 export const firebaseDB = firebaseUtil.database();
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
