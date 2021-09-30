0bacb2afb07c4b2bf2d2b33715e2cbb97bcccd94
fatih <fatihenaz@gmail.com>
2021-09-30 21:29:18 +0300

    Created context for authentication data

--- src/context/AuthContextProvider.js
+++ src/context/AuthContextProvider.js
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, { useContext, createContext } from 'react';
+
+// Create context for authentication data
+const AuthContext = createContext();
+
+// Define a function to get data from Auth context 
+function useAuth() {
+    return useContext(AuthContext)
+}
 
 const AuthContextProvider = () => {
     return (
