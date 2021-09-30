20b6c25e1d9e80646c77b34274f8a4bdcf967505
fatih <fatihenaz@gmail.com>
2021-09-30 21:48:23 +0300

    update AutContext

--- src/context/AuthContextProvider.js
+++ src/context/AuthContextProvider.js
@@ -1,28 +1,56 @@
-import React, { useContext, createContext } from 'react';
-import {auth, googleProvider} from "../utils/firebaseUtil"
+import React, { useContext, createContext } from "react";
+import { auth, googleProvider } from "../utils/firebaseUtil";
 
-// Create context for authentication data
+//! Create context for autentication data
 const AuthContext = createContext();
 
-// Define a function to get data from Auth context 
+//! Define a function to get data from Auth context
 export function useAuth() {
-    return useContext(AuthContext)
+  return useContext(AuthContext);
 }
 
-const AuthContextProvider = () => {
-    function signup(email, password) {
-        return auth.createUserWithEmailAndPassword(email,password)
-    }
+const AuthContextProvider = ({children}) => {
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
 
-    function login(email, password) {
-        return auth.signInWithEmailAndPassword(email,password)
-    }
-    
-    return (
-        <div>
-            
-        </div>
-    )
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    auth.signOut();
+  }
+
+  function loginWithGoogle() {
+    googleProvider.setCustomParameters({ prompt: "select_account" });
+    auth.signInWithPopup(googleProvider);
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
+    const values = {
+        signup,
+        login,
+        logout,
+        resetPassword,
+        updatePassword,
+        updateEmail,
+        loginWithGoogle
+        
 }
+    return <AuthContext.Provider value={values}>
+      {children}
+  </AuthContext.Provider>;
+};
 
 export default AuthContextProvider;
