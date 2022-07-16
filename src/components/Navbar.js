07d1659f207c0f06ddd17f321816f44ecdee76ac
fatih <fatihenaz@gmail.com>
2022-07-16 22:23:41 +0300

    implemented context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,15 @@
-import React, { createContext } from "react";
+import React  from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
 import cwLogo from "../assets/cw.jpeg";
 import { useAuth } from "../context/AuthContextProvider";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
--- src/context/AuthContextProvider.js
+++ src/context/AuthContextProvider.js
@@ -2,7 +2,6 @@ import { TvRounded } from "@material-ui/icons";
 import React, { useContext, createContext, useState, useEffect } from "react";
 import { auth, googleProvider } from "../utils/firebaseUtil";
 
-
 // Create context for autentication data
 const AuthContext = createContext();
 
@@ -11,21 +10,19 @@ export function useAuth() {
   return useContext(AuthContext);
 }
 
-
 const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
-    const [currentUser, setCurrentUser] = useState();
-    const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            setCurrentUser(user);
-            setLoading(false)
-        })
-        return unsubscribe;
-    }, [])
-    
-    function signup(email, password) {
+  function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
   }
 
@@ -53,20 +50,22 @@ const AuthContextProvider = ({ children }) => {
   function updatePassword(password) {
     return currentUser.updatePassword(password);
   }
-    const values = {
-        currentUser,
-        signup,
-        login,
-        logout,
-        resetPassword,
-        updatePassword,
-        updateEmail,
-        loginWithGoogle  
-  }
-  
-    return  <AuthContext.Provider value={values}>
-              {!loading && children}
-            </AuthContext.Provider>;
+  const values = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    resetPassword,
+    updatePassword,
+    updateEmail,
+    loginWithGoogle,
+  };
+
+  return (
+    <AuthContext.Provider value={values}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContextProvider;
